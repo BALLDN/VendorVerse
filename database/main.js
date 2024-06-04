1d8c4853cd219cb6fb593d4a42a3a472782d8361
Lorcan Ryan <lorcanry03@gmail.com>
2024-06-04 12:07:20 +0100

    Initial database functionality

---
+++ database/main.js
@@ -0,0 +1,41 @@
+// main.js
+import { signUp, signIn, signOutUser } from './database/auth.js';
+
+document.getElementById('signUpButton').addEventListener('click', () => {
+  const email = document.getElementById('Email').value;
+  const password = document.getElementById('Password').value;
+  const status = document.getElementById('Status').value;
+  const user_type = document.getElementById('User_type').value;
+
+  signUp(email, password, { status, user_type })
+    .then(() => {
+      alert('User signed up and data stored in Firestore');
+    })
+    .catch((error) => {
+      alert('Error: ' + error.message);
+    });
+});
+
+document.getElementById('signInButton').addEventListener('click', () => {
+  const email = document.getElementById('Email').value;
+  const password = document.getElementById('Password').value;
+  
+  signIn(email, password)
+    .then((userData) => {
+      console.log(userData);
+      alert('User signed in');
+    })
+    .catch((error) => {
+      alert('Error: ' + error.message);
+    });
+});
+
+document.getElementById('signOutButton').addEventListener('click', () => {
+  signOutUser()
+    .then(() => {
+      alert('User signed out');
+    })
+    .catch((error) => {
+      alert('Error: ' + error.message);
+    });
+});

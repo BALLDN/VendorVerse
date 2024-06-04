fefb11b5b2da53fd60e6072377885a5dc7b40964
DylanSwift56 <dylanswift56@gmail.com>
2024-06-04 12:20:58 +0100

    All home pages created, forgot password, and vendor details form created

--- app.py
+++ app.py
@@ -1,24 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask import jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('public_home_page.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        return redirect(url_for('vendor_details_page'))
     return render_template('login.html')
+
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
-@app.route('/home')
+@app.route('/vendor')
 def home():
-    return render_template('home_page.html')
+    return render_template('vendor_home_page.html')
+
+@app.route('/employee')
+def employee():
+    return render_template('employee_home_page.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin_home_page.html')
 
 @app.route('/reset')
 def reset():
     return render_template('forgot_password.html')
+
+@app.route('/vendor_details')
+def vendor_details_page():
+    return render_template('vendor_details_page.html')

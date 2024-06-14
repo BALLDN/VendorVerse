ccf010437854d61138f50179e72f519e8429bf50
adhl-0216 <113506757+adhl-0216@users.noreply.github.com>
2024-06-14 13:17:08 +0100

    Update upload_artifact.py

--- .github/workflows/upload_artifact.py
+++ .github/workflows/upload_artifact.py
@@ -2,14 +2,27 @@ import sys
 import os
 import requests
 
-def upload_zip_file(username, api_token, artifact_path):
-    # Construct the upload URL based on PythonAnywhere API documentation
+def main():
+    if len(sys.argv) < 4:
+        print("Usage: python ./github/workflows/upload_artifact.py PA_USERNAME PA_API_TOKEN ARTIFACT_PATH")
+        sys.exit(1)
+
+    pa_username = sys.argv[1]
+    pa_api_token = sys.argv[2]
+    artifact_path = sys.argv[3]
+    try:
+        upload_artifact(pa_username, pa_api_token, artifact_path)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+def upload_artifact(username, api_token, artifact_path):
     api_base_url = "https://eu.pythonanywhere.com/api/v0"
     upload_url = f"{api_base_url}/user/{username}/files/path/home/{username}/web_app/{artifact_path}"
 
     # Ensure the zip file exists
     if not os.path.exists(artifact_path):
-        print(f"Error: Zip file '{artifact_path}' not found.")
+        print(f"Error: Artifact '{artifact_path}' not found.")
         return
 
     # Open the zip file to upload
@@ -20,32 +33,10 @@ def upload_zip_file(username, api_token, artifact_path):
         try:
             response = requests.post(upload_url, headers=headers, files=files)
             response.raise_for_status()
-            print(f"File uploaded successfully. Status code: {response.status_code}")
+            print(f"Artifact uploaded successfully. Status code: {response.status_code}")
         except requests.exceptions.RequestException as e:
-            msg = f"Error uploading file: {e}"
-            print(msg)
+            msg = f"Error uploading Artifact: {e}"
             raise Exception(msg)
 
 if __name__ == "__main__":
-
-    username = sys.argv[1]
-    if username:
-        print("No username provided")
-        print("Usage: python upload_artifact.py <username> <api_token> <artifact_path>")
-        sys.exit(1)
-    api_token = sys.argv[2]
-    if api_token:
-        print("No api_token provided")
-        print("Usage: python upload_artifact.py <username> <api_token> <artifact_path>")
-        sys.exit(1)
-    artifact_path = sys.argv[3]
-    if artifact_path:
-        print("No artifact_path provided")
-        print("Usage: python upload_artifact.py <username> <api_token> <artifact_path>")
-        sys.exit(1)
-
-    try:
-        upload_zip_file(username, api_token, artifact_path)
-    except Exception as e:
-        print(e)
-        sys.exit(1)
+    main()

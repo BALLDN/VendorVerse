682c0092df62264f90170c561aa8ee1299db6697
adhl-0216 <hanlim2002@gmail.com>
2024-06-18 23:21:13 +0000

    Cleaned up root directory

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import firebase_admin
-<<<<<<< HEAD
 from models.user_model import User
 from models.booking_model import bookings
-=======
-from user_model import users
-from booking_model import bookings
->>>>>>> 6943fd0 (Create Booking page is now functional for vendors)
 from firebase_admin import credentials
 from firebase_admin import firestore
 from flask import Flask, flash, redirect, render_template, request, url_for, make_response
@@ -16,25 +10,6 @@ app = Flask(__name__)
 # Put in to Flash Error Message, need to improve sessions later
 app.secret_key = "Secret Key"
 
-=======
-<<<<<<< Updated upstream
-from flask import Flask, redirect, render_template, request, url_for
-from flask import jsonify
-=======
-import firebase_admin
-from user_model import users
-from firebase_admin import credentials
-from firebase_admin import firestore
-<<<<<<< HEAD
-from flask import Flask, redirect, render_template, request, url_for
->>>>>>> Stashed changes
-=======
-from flask import Flask, flash, redirect, render_template, request, url_for, make_response
->>>>>>> bcbbbc5 (Cookies pull username from login from and user type from database connected to user.)
-
-app = Flask(__name__)
-
->>>>>>> 45527d8 (Registration Page now Functional)
 # Use the application default credentials.
 
 cred = credentials.Certificate("serviceAccount.json")
@@ -46,25 +21,17 @@ db = firestore.client()
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-<<<<<<< HEAD
         User.get_users(db)
 
         # Get Cookies containing login info
-=======
-        users.get_users(db)
-
-        #Get Cookies containing login info
->>>>>>> bcbbbc5 (Cookies pull username from login from and user type from database connected to user.)
         login_email = request.cookies.get('login_email')
 
         if login_email:
             return render_template('login.html', login_email=login_email)
-<<<<<<< HEAD
 
         return render_template('login.html')
 
     if request.method == 'POST':
-<<<<<<< HEAD
 
         # Get username used in login form
         login_email = request.form['Email']
@@ -74,40 +41,15 @@ def login():
         elif (User.validate_user(db) == "E"):
             url_response = make_response(redirect(url_for('employee')))
         elif (User.validate_user(db) == "A"):
-=======
-        
-        return render_template('login.html')
-        
-        
-    if request.method == 'POST':
-
-        #Get username used in login form
-        login_email = request.form['Email']
-
-
-        if(users.validate_user(db) == "V"):
-            url_response = make_response(redirect(url_for('vendor')))
-        elif(users.validate_user(db) == "E"):
-            url_response = make_response(redirect(url_for('employee')))
-        elif(users.validate_user(db) == "A"):
->>>>>>> bcbbbc5 (Cookies pull username from login from and user type from database connected to user.)
             url_response = make_response(redirect(url_for('admin')))
         else:
             # Error Message displays as appropriate
             flash(User.validate_user(db))
             return render_template('login.html')
-<<<<<<< HEAD
 
         # Set cookies for login details + user type
         url_response.set_cookie('login_email', login_email)
         url_response.set_cookie('user_type', User.validate_user(db))
-=======
-        
-        
-        #Set cookies for login details + user type
-        url_response.set_cookie('login_email',login_email)
-        url_response.set_cookie('user_type',users.validate_user(db))
->>>>>>> bcbbbc5 (Cookies pull username from login from and user type from database connected to user.)
 
         return url_response
     return render_template('login.html')
@@ -116,20 +58,13 @@ def login():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-<<<<<<< HEAD
         User.add_user(db)
 
         # Get username + user_type used in register form
-=======
-        users.add_user(db)
-
-        #Get username + user_type used in register form
->>>>>>> bcbbbc5 (Cookies pull username from login from and user type from database connected to user.)
         login_email = request.form['Email']
         user_type = request.form['User_Type']
 
         if user_type == "Vendor":
-<<<<<<< HEAD
             url_response = make_response(
                 redirect(url_for('vendor_details_page')))
             flash("Your Account is Pending Approval")
@@ -145,42 +80,6 @@ def register():
 
         return url_response
 
-=======
-<<<<<<< Updated upstream
-        return redirect(url_for('vendor_details_page'))
-    return render_template('login.html')
-
-
-@app.route('/register')
-=======
-        users.get_users(db)
-        return redirect(url_for('home'))
-
-@app.route('/register', methods=['GET', 'POST'])
->>>>>>> Stashed changes
-def register():
-    if request.method == 'POST':
-        users.set_user(db)
-        return redirect(url_for('home'))
-        
->>>>>>> 45527d8 (Registration Page now Functional)
-=======
-            url_response = make_response(redirect(url_for('vendor_details_page')))
-            flash("Your Account is Pending Approval")
-        
-        else:
-            url_response = make_response(redirect(url_for('register')))
-         
-
-        flash("Your Account is Pending Approval")
-        
-        #Set cookies for login details + user type
-        url_response.set_cookie('login_email',login_email)
-        url_response.set_cookie('user_type',users.validate_user(db))
-
-        return url_response
-        
->>>>>>> bcbbbc5 (Cookies pull username from login from and user type from database connected to user.)
     return render_template('register.html')
 
 
@@ -193,13 +92,9 @@ def index():
 def vendor():
     return render_template('vendor_home_page.html')
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 6943fd0 (Create Booking page is now functional for vendors)
 @app.route('/create_booking', methods=['GET', 'POST'])
 def create_booking():
-<<<<<<< HEAD
     if request.method == 'GET':
         bookings.get_user_id(db, request.cookies.get('login_email'))
     if request.method == "POST":
@@ -207,36 +102,12 @@ def create_booking():
         bookings.add_booking(db, user_id)
         flash("Your Booking has been created and is pending approval")
         return render_template('create_booking_vendor.html')
-<<<<<<< HEAD
-=======
-    else:
-        return render_template('create_booking_admin.html')
-    '''
-=======
->>>>>>> 853b9f4 (Create_Booking_Admin Page Working)
-    if request.method == 'GET':
-        bookings.get_user_id(db, request.cookies.get('login_email'))
-    if request.method == "POST":
-        user_id = bookings.get_user_id(db, request.cookies.get('login_email'))
-        bookings.add_booking(db, user_id)
-        flash("Your Booking has been created and is pending approval")
-        return render_template('create_booking_vendor.html')
-<<<<<<< HEAD
-        
-            
-        
->>>>>>> 6943fd0 (Create Booking page is now functional for vendors)
-=======
->>>>>>> 853b9f4 (Create_Booking_Admin Page Working)
     return render_template('create_booking_vendor.html')
 
 
 '''Only Temporary until sessions are introduced'''
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 853b9f4 (Create_Booking_Admin Page Working)
 @app.route('/create_booking_admin', methods=['GET', 'POST'])
 def create_booking_admin():
     if request.method == 'GET':
@@ -246,6 +117,11 @@ def create_booking_admin():
         bookings.add_booking(db, user_id)
         return redirect(url_for('create_booking_admin'))
     return render_template('create_booking_admin.html')
+
+
+@app.route('/manage_booking')
+def manage_bookings():
+    return render_template('manage_bookings_page.html')
 
 
 @app.route('/employee')

9f14c02141ed258d31a2f9b3010f603f9f95e1b3
DylanSwift56 <dylanswift56@gmail.com>
2024-06-24 15:56:55 +0100

    Manage_booking_page worked on

--- models/booking_model.py
+++ models/booking_model.py
@@ -15,6 +15,9 @@ class Booking:
         print(self.date, self.deal, self.location,
               self.additional_info, self.vendor_id, self.status)
 
+    def get_date(self):
+        return self.date
+
     @staticmethod
     def add_booking(database_connection, vendor_id):
 
@@ -28,7 +31,7 @@ class Booking:
         additional_info = request.form['additional_info']
 
         '''In case discount checkbox isn't checked'''
-        if request.form.get('discount_checkbox') is None or request.form('discount') is None:
+        if request.form.get('discount_checkbox') is None or request.form['discount'] is None:
             deal = "No Discount"
         else:
             deal = request.form['discount']
@@ -66,25 +69,50 @@ class Booking:
 
     def get_bookings_by_vendor_id(database_connection, vendor_id):
         booking_ref = database_connection.collection('Bookings')
-        docs = booking_ref.get()
-        for doc in docs:
-            print('{} => {}'.format(doc.id, doc.to_dict()))
-            return ('{} => {}'.format(doc.id, doc.to_dict()))
-        return docs
+        query = booking_ref.where("Vendor_ID", "==", vendor_id)
+        results = query.get()
+        return results
+
+    def get_approved_bookings(database_connection):
+        booking_ref = database_connection.collection('Bookings')
+        query = booking_ref.where("Status", "==", "A")
+        results = query.get()
+        return results
 
     def remove_booking(database_connection, booking_id):
 
-        booking_ref = database_connection.collection('Users')
+        booking_ref = database_connection.collection(
+            'Bookings').document(booking_id)
+        print(booking_id)
+        if booking_ref is not None:
+            print(booking_ref)
+            booking_ref.update({"Status": "D"})
+            return booking_ref
+        else:
+            return "No User Found!"
 
-        query = booking_ref.where(__name__, "==", booking_id)
-        results = query.get()
+    def modify_booking(database_connection, booking_id):
+        booking_ref = database_connection.collection(
+            'Bookings').document(booking_id)
+        if booking_ref is not None:
+            if (request.cookies.get('user_type') == "V"):
+                status = "P"
+            else:
+                status = "A"
+            date = request.form['date']
+            location = request.form['location']
+            additional_info = request.form['additional_info']
 
-        for doc in results:
-            user_id = doc.id
+            '''In case discount checkbox isn't checked'''
+            if request.form['discount'] is None:
+                discount = "No Discount"
+            else:
+                discount = request.form['discount']
 
-        if len(results) > 0:
-            print("found")
-            print(user_id)
-            return user_id
+            print(booking_ref)
+            booking_ref.update({"Status": status, "Date": date, "Location": location,
+                               "Deal": discount, "`Additional Info`": additional_info})
+            return booking_ref
+
         else:
             return "No User Found!"

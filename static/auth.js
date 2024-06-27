92198efda37e654e02c5b11a800b012cd6c165d2
DylanSwift56 <114220425+DylanSwift56@users.noreply.github.com>
2024-06-27 16:27:38 +0100

    Revert "Jip 113 securely store user details"

--- static/auth.js
+++ static/auth.js
@@ -1,7 +1,7 @@
 // auth.js
 console.log('login.js is loaded');
 
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-auth.js";
 import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js";
 import { auth, firestore } from './firebase.js'; // Assuming firebase.js is in the same directory
 
@@ -27,62 +27,28 @@ export const signUp = (email, password, status, user_type) => {
     });
 };
 
-// export const signIn = (email, password) => {
-//   return signInWithEmailAndPassword(auth, email, password)
-//     .then((userCredential) => {
-//       const user = userCredential.user;
-//       console.log('User signed in:', user);
+export const signIn = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log('User signed in:', user);
 
-//       return getDoc(doc(firestore, "Users", user.uid));
-//     })
-//     .then((docSnap) => {
-//       if (docSnap.exists()) {
-//         console.log('User data:', docSnap.data());
-//         return docSnap.data();
-//       } else {
-//         console.log('No such document!');
-//         return null;
-//       }
-//     })
-//     .catch((error) => {
-//       console.error('Error signing in:', error.code, error.message);
-//       throw error;
-//     });
-// };
-
-document.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('Email').value;
-  const password = document.getElementById('Password').value;
-  const auth = getAuth();
-
-
-  // Sign in with Firebase Authentication
-  signInWithEmailAndPassword(auth, email, password)
-    .then(function (userCredential) {
-      return userCredential.user.getIdToken();
+      return getDoc(doc(firestore, "Users", user.uid));
     })
-    .then(function(idToken) {
-      console.log(idToken)
-
-      //Send ID token to your backend
-      return fetch('/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          'idToken': idToken })
-      });
+    .then((docSnap) => {
+      if (docSnap.exists()) {
+        console.log('User data:', docSnap.data());
+        return docSnap.data();
+      } else {
+        console.log('No such document!');
+        return null;
+      }
     })
-    .then(function(response) {
-      return response
-    })
-    .catch(function (error) {
-      console.error('Error:', error.code, error.message);
+    .catch((error) => {
+      console.error('Error signing in:', error.code, error.message);
+      throw error;
     });
-});
-
+};
 
 export const signOutUser = () => {
   return signOut(auth)

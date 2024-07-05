15f75d7917758515c580c88097d70d4918906850
DylanSwift56 <dylanswift56@gmail.com>
2024-07-05 14:43:15 +0100

    Vendor Details Page now working

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from wtforms import StringField, TextAreaField, DateField
 from wtforms.validators import InputRequired
 from models.user_model import User
 from models.booking_model import Booking
+from models.vendor_model import Vendor
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
@@ -246,8 +247,15 @@ def reset():
     return render_template('forgot_password.html')
 
 
-@app.route('/vendor_details')
+@app.route('/vendor_details', methods=['GET','POST'])
 def vendor_details_page():
+    email = request.cookies.get('login_email')
+    user_id = User.get_user_id_from_email(db, email)
+    if request.method == 'POST':
+        Vendor.add_vendor_details(db, user_id)
+        flash("Your Details have been saved and your account is pending approval")
+        return redirect(url_for('index'))
+        
     return render_template('vendor_details_page.html')
 
 
---
+++ models/vendor_model.py
@@ -0,0 +1,30 @@
+from flask import request
+
+
+class Vendor:
+
+    def __init__(self,vendor_name, phone_number, address, about_us, user_id):
+        self.about_us = about_us
+        self.address = address
+        self.phone_number = phone_number
+        self.user_id = user_id
+        self.vendor_name = vendor_name
+
+    def display(self):
+        print("")
+        
+    def add_vendor_details(database_connection, user_id):
+        # adds a vendors details to db
+        vendor_name = request.form['vendor_name']
+        phone_number = request.form['phone_number']
+        address = request.form['address']
+        about_us = request.form['about_us']
+
+        vendor_details = Vendor(vendor_name, phone_number, address, about_us, user_id)
+
+        details = {"Vendor_Name": vendor_details.vendor_name, "Phone_Number": vendor_details.phone_number,
+                "Address": vendor_details.address, "About_Us": vendor_details.about_us, "User_ID": user_id}
+        database_connection.collection("Vendors").add(details)
+        return details
+        
+    
